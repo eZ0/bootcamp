60484ddeb5cec057fcf0660287e4a36dd4167455
eZ0 <kusksu@gmail.com>
2015-10-09 16:25:16 +0200

    Unit tests with Karma

---
+++ ToDo/src/scripts/modules/util.spec.js
@@ -0,0 +1,41 @@
+describe('util service', function(){
+
+    describe('pluralize', function(){
+
+        it("should return bananas for 2 banana", function() {
+            var result = util.pluralize(2, "banana");
+            expect(result).toBe("bananas");
+        });
+
+        it("should return banana for 1 banana", function() {
+            var result = util.pluralize(1, "banana");
+            expect(result).toBe("banana");
+        });
+
+        it("should return banana for '' & banana", function() {
+            var result = util.pluralize('',"banana");
+            expect(result).toBe("banana");
+        });
+
+        it("should return banana for '2' banana", function() {
+            var result = util.pluralize('2',"banana");
+            expect(result).toBe("bananas");
+        });
+
+    });
+
+    describe('uuid', function(){
+        it("should return random unique string", function() {
+            var result1 = util.uuid();
+            var result2 = util.uuid();
+            expect(result1).not.toBe(result2);
+        });
+
+        it("should return string of the same length", function() {
+            var result1 = util.uuid();
+            var result2 = util.uuid();
+            expect(result1.length).toBe(result2.length);
+        });
+    });
+
+});
