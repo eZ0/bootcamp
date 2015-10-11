6e86543f87cffbe3c91b6cd27ff9ff88898f488b
eZ0 <kusksu@gmail.com>
2015-10-11 13:16:45 +0200

    correct bundel.js version for js

--- ToDo/part_2/src/bundle.js
+++ ToDo/part_2/src/bundle.js
@@ -9499,15 +9499,16 @@
 
 /***/ },
 /* 3 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	
 	'use strict';
-	// var util = require("./util.js");
 
 	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	var util = __webpack_require__(2);
 
 	var TodoRepo = (function () {
 
